f9ee62fc37f403ff4eab99e0b962053433c60862
MateusCecchin <mateusfernandescn@gmail.com>
2023-01-28 23:37:02 -0400

    refactor: corrige header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,6 +1,6 @@
 export function Header() {
   return (
-    <div className="min-h-[64px] flex items-center justify-center sm:justify-start px-40 shadow-2xl bg-slate-700">
+    <div className="min-h-[64px] flex items-center justify-center sm:justify-start md:px-40 shadow-2xl bg-slate-700">
       <h1 className="md:text-2xl font-semibold whitespace-nowrap">
         What country is this
       </h1>
